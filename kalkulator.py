9df1a858335d528a713a84c7a4fa8ec5bb39877b
lukasz <lukaszstawirowski@wp.pl>
2020-05-21 14:57:45 +0200

    gui commit

--- kalkulator.py
+++ kalkulator.py
@@ -4,8 +4,12 @@ def get_info():
 def dodaj(a, b):
         wynik = a + b
         return wynik
+
+def odejmi j(a, b):
     
 get_info()
 a = int(input())
 b = int(input())
 print(dodaj(a, b))
+
+print('Koniec programu, papa!!')
